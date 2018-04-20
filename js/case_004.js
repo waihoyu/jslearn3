37ec8b84cada12ed399fa08b4ac67d38bbf16888
waihoyu <13701343809@qq.com>
2018-04-20 23:42:03 +0800

    代理模式自学案例

---
+++ js/case_004.js
@@ -0,0 +1,51 @@
+console.log("测试代码")
+
+var xiaoming = {
+
+    name: '小明',
+    age: 21,
+    hasGirlFriend: false,
+    sendFlower: function (target) {
+        var flower = new Flower("满天星");
+        if (typeof target.receiveFlower == 'function')
+            target.receiveFlower(flower,this.name);
+        else
+            console.log("没有送出鲜花");
+    }
+}
+
+
+var xiaomei = {
+
+    name: '小美',
+    age: 23,
+    hasBoyFriend:false,
+    listenGoodMood:function(fn){
+        setTimeout(function(){
+            fn();
+        },10000);
+    },
+    receiveFlower:function (flower,name) {
+        console.log('收到' + name + '的' + flower.type);
+    }
+}
+
+var  xiaoxue = {
+    name: "小雪",
+    age: 22,
+    receiveFlower: function (flower,name) {
+        xiaomei.listenGoodMood(function () {
+            xiaomei.receiveFlower(flower,name);
+        })
+    }
+}
+
+
+var Flower = function (type) {
+    this.type = type || '玫瑰';
+}
+
+xiaoming.sendFlower(xiaoxue);
+
+
+
